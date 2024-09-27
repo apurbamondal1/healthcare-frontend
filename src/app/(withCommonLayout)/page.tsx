09f1e6888bdc08261c3b44d75c580a02eb82ea7e
unknown <apurba67141@gmail.com>
2024-09-27 12:52:17 +0530

    first commit

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>home page</h1>
+      <Button>Contained</Button>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+
+
+const login = () => {
+    return (
+        <div>
+          <h1>login here</h1>  
+        </div>
+    );
+};
+
+export default login;
